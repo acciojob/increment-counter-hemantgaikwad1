40b4a714d9b86eb8a8f9fe3e84964e0af0917b01
Hemant Gaikwad <103812753+hemantgaikwad1@users.noreply.github.com>
2026-08-11 00:46:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let btn=document.getElementById('incrementBtn');
+let counter=document.getElementById('counter');
+
+btn.onclick=()=>{
+	counter.innerText=Number(counter.innerText)+1;
+}
+alert(counter.innerText);
